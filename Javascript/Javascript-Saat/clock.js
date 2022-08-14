efef8fa4ff4589f33cae72b9acfd00fc61375bf9
mehmet <mehmet_kirsakal@hotmail.com>
2022-08-15 01:57:48 +0300

    javascript odev1 updated

--- Javascript/Javascript-Saat/clock.js
+++ Javascript/Javascript-Saat/clock.js
@@ -3,14 +3,19 @@
 let name = prompt("Lütfen adınızı girin..");
 
 let myName = document.querySelector("#myName");
-myName.innerHTML = name
 
+if(name){
+    myName.innerHTML = name;
+}else{
+    alert("Adınız boş olamaz!");
+    location.reload();
+}
 
-
+let days = ["Pazar", "Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
 function showTime(){
     let date = new Date();
     let info = document.querySelector("#myClock");
-    info.innerHTML = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+    info.innerHTML = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" "+ days[date.getDay()];
     console.log(info.innerHTML)
 
 }
